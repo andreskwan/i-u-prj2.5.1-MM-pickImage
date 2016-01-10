e3622efccd595102223a24e25c7aa385ac3ffba3
ANDRES KWAN <andreskwan@gmail.com>
2016-01-10 12:26:32 -0500

    Added - SentMemeCollectionViewCell
    - SentMemesCollectionVC
    -- viewWillAppear
    -- numberOfItemsInSection
    -- cellForTimetAtIndexPath
    
    - Storyboard
    -- titles for collection and table views
    -- CollectionViewCell

--- MM1.1PickingAnImage/Controllers/SentMemesCollectionViewController.swift
+++ MM1.1PickingAnImage/Controllers/SentMemesCollectionViewController.swift
@@ -15,4 +15,26 @@ class SentMemesCollectionViewController: UICollectionViewController {
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         memes = appDelegate.memes
     }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        memes = appDelegate.memes
+        collectionView?.backgroundColor = UIColor.whiteColor()
+        collectionView?.reloadData()
+    }
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memes.count
+    }
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CustomMemeCell", forIndexPath: indexPath) as! SentMemeCollectionViewCell
+        let meme = memes[indexPath.item]
+        cell.memeImage.image = meme.memedImage
+        return cell
+    }
+    @IBAction func showMemeEditor(sender: AnyObject) {
+        let storyboard = UIStoryboard (name: "Main", bundle: nil)
+        let memeEditorVC = storyboard.instantiateViewControllerWithIdentifier("NavigationForMemeEditor")
+        self.presentViewController(memeEditorVC, animated: true, completion: nil)
+    }
+    
 }
